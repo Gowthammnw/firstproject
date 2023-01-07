33204c6e9e198cb46759a905dd559baae9a6e358
Gowthammnw <122154655+Gowthammnw@users.noreply.github.com>
2023-01-07 17:26:00 +0530

    change into hellow java

--- Apps.java
+++ Apps.java
@@ -3,7 +3,7 @@ public class App{
 
 	public static void main(String[] args){
 	System.out.println("hellow world");
-	System.out.println("hellow world");
+	System.out.println("hellow java");
 	
 }
 }
